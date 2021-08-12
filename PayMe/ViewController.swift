74998fa44d000b1de5195e4ac15a0490ebe60c65
huajustin <huajustinh@gmail.com>
2021-08-12 09:45:27 -0700

    Initial Commit

---
+++ PayMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayMe
+//
+//  Created by Justin Hua on 2021-08-12.
+//  Copyright © 2021 Justin Hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
